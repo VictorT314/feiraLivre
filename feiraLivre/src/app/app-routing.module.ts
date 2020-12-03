634d32bcb51c4170df8d4372375fff586d593e37
Bruna dos Santos <brusnts@outlook.com>
2020-12-02 21:12:09 -0300

    configurando pagina Sobre Nós

--- feiraLivre/src/app/app-routing.module.ts
+++ feiraLivre/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch: 'full', redirectTo:'home'},
+  {path:'home', component: HomeComponent},
+  {path:'sobre-nos', component: SobreNosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
