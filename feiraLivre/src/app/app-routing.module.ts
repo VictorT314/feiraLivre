253c1d27d46c7783300185a2ec6473eb4631d9ab
Matheus <you@example.com>
2020-12-07 10:39:53 -0300

    sobre nos

--- feiraLivre/src/app/app-routing.module.ts
+++ feiraLivre/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'sobre-nos', component: SobreNosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- feiraLivre/src/app/app.module.ts
+++ feiraLivre/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
