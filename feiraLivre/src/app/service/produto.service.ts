a39396367b7cdafaa1261b6233f5e7302d008586
Victor Tavares <victor.as156@gmail.com>
2020-12-10 09:07:04 -0300

    Get e Post para produtos e preparação para carrinho

---
+++ feiraLivre/src/app/service/produto.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../model/Produto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token')!)
+  }
+
+  getAllProdutos(): Observable<Produto[]>{
+    return this.http.get<Produto[]>('http://localhost:8080/produto', this.token)
+  }
+}
