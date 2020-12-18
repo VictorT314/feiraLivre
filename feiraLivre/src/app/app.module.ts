02ea36c756b5e7ab178b8fb1979df485fa7e7ef0
Matheus <you@example.com>
2020-12-18 00:18:02 -0300

    alterando modal do footer e endereço

--- feiraLivre/src/app/app.module.ts
+++ feiraLivre/src/app/app.module.ts
@@ -1,10 +1,8 @@
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { ModalModule } from 'ngx-bootstrap/modal';
-
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
