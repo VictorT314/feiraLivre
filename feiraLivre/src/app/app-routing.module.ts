179d83b616a7e51286ee6f13c7bd1353cacbdcae
unknown <zainequeiros@gmail.com>
2020-12-18 01:17:22 -0300

    Página de Políticas

--- feiraLivre/src/app/app-routing.module.ts
+++ feiraLivre/src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { HomeComponent } from './home/home.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { PoliticasComponent } from './politicas/politicas.component'
+
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo:'home'},
   {path:'home', component: HomeComponent},
   {path:'sobre-nos', component: SobreNosComponent},
   {path: 'contatos', component: ContatosComponent},
   {path: 'catalogo', component: ProdutosComponent},
-  {path: 'carrinho', component: CarrinhoComponent}
+  {path: 'carrinho', component: CarrinhoComponent},
+  {path: 'politicas', component: PoliticasComponent}
 
 ];
 @NgModule({
